8b68191eac0950b049d42bd7fa8237aac66d7a0c
Teungtran <nguyentrantrung2504@gmail.com>
2024-08-04 13:25:39 +0700

    stock_price_visualizing.py

--- stock_price_visualizing.py
+++ stock_price_visualizing.py
@@ -2,9 +2,6 @@ import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.layers import Dense, Dropout, LSTM
-from tensorflow.keras.models import Sequential
 import datetime as dt
 import seaborn as sns
 # get the stock
